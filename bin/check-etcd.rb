33c13178411465e10606e59cf1413311e7e38f9f
Guillaume Coré <fridim@onfi.re>
2015-08-27 11:32:00 +0200

    fix hash syntax warnings

--- bin/check-etcd.rb
+++ bin/check-etcd.rb
@@ -79,10 +79,10 @@ class EtcdNodeStatus < Sensu::Plugin::Check::CLI
 
     r = RestClient::Resource.new("#{protocol}://#{config[:server]}:#{config[:port]}/v2/stats/self",
                                  timeout: 5,
-                                 :ssl_client_cert  => (OpenSSL::X509::Certificate.new(File.read(config[:cert])) unless config[:cert].nil?),
-                                 :ssl_client_key   => (OpenSSL::PKey::RSA.new(File.read(config[:key]), config[:passphrase]) unless config[:key].nil?),
-                                 :ssl_ca_file      =>  config[:ca],
-                                 :verify_ssl       =>  config[:insecure] ? 0 : 1
+                                 ssl_client_cert: (OpenSSL::X509::Certificate.new(File.read(config[:cert])) unless config[:cert].nil?),
+                                 ssl_client_key: (OpenSSL::PKey::RSA.new(File.read(config[:key]), config[:passphrase]) unless config[:key].nil?),
+                                 ssl_ca_file:  config[:ca],
+                                 verify_ssl:  config[:insecure] ? 0 : 1
                                 ).get
     if r.code == 200
       ok 'etcd is up'
